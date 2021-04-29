be3e54f0ae0deff8e18a29445efdc2e215c1549d
Kanisorn Sutham <kanisorns.k@gmail.com>
2021-04-29 16:28:05 +0700

    feat: init nextjs

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <main className="mb-16">
+        <h1 className="mb-4 text-3xl font-bold tracking-tight text-black md:text-5xl">
+          Hey! TEDx Charoenkrung
+        </h1>
+      </main>
+    </div>
+  );
+}
